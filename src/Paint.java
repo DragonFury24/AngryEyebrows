e38f5ed904118672db9d110e4d69183c5b111208
DragonFury24 <dennisdnguyen19@gmail.com>
2017-11-14 10:23:42 -0500

    getDistance method

--- src/Paint.java
+++ src/Paint.java
@@ -86,6 +86,9 @@ public class Paint {
 //        return Math.sqrt()
 //    }
 
+    private int getDistance(int x1, int y1, int x2, int y2) {
+        return (int)Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
     private int getLinearY(int x) {
         return x * slope + yIntercept;
     }
