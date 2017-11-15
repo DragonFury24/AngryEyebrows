673a8856a32a7865ee67d3ab3b5b651e938d13e8
DragonFury24 <dennisdnguyen19@gmail.com>
2017-11-15 09:40:26 -0500

    .

--- src/AngryEyebrows.java
+++ src/AngryEyebrows.java
@@ -1,13 +1,13 @@
+import java.io.File;
+
 public class AngryEyebrows {
     public static void main(String[] args) {
-        String fName = "src/angrybirdwithouteyebrow.jpg";
-        Picture picture = new Picture(fName);
+        Picture picture = new Picture(new File("src").exists() ? "src/angrybirdwithouteyebrow.jpg" : "angrybirdwithouteyebrow.jpg");
         picture.show();
         Paint painter = new Paint(picture);
 
-        for (int i = 338; i <= 347; i++) {
+        for (int i = 338; i <= 347; i++)
             painter.setPixelColor(i, 273);
-        }
 
         painter.drawLine(200, 200, 450, 450);
         picture.explore();         //allows to search color by pixel
